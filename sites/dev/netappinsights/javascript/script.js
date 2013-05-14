af1806507212aca0297d62e75f9b438fa6c3e9d8
Noah Glushien <nglushien@techtarget.com>
2013-05-14 14:43:09 -0400

    Updated Landing Page, What Is This drop down

--- sites/dev/netappinsights/javascript/script.js
+++ sites/dev/netappinsights/javascript/script.js
@@ -37,26 +37,26 @@ $(document).ready(function () {
 //      })
 // });
 
-// $(function(){
+$(function(){
 
-//     var url = window.location.pathname, 
-//         urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
-//         // now grab every link from the navigation
-//         $('.leftColumnNav a').each(function(){
-//             // and test its normalized href against the url pathname regexp
-//             if(urlRegExp.test(this.href.replace(/\/$/,''))){
-//                 $(this).parent().addClass('active');
-//             }
-//         });
-
-// });
-
-$(function() {
-  var pageURL=window.location.pathname; 
-
-  $(".leftColumnNav a[href*='"+pageURL+"']").parent().addClass("active");
+    var url = window.location.pathname, 
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        // now grab every link from the navigation
+        $('.leftColumnNav a').each(function(){
+            // and test its normalized href against the url pathname regexp
+            if(urlRegExp.test(this.href.replace(/\/$/,''))){
+                $(this).parent().addClass('active');
+            }
+        });
 
 });
+
+// $(function() {
+//   var pageURL=window.location.pathname; 
+
+//   $(".leftColumnNav a[href*='"+pageURL+"']").parent().addClass("active");
+
+// });
 
 // For What Is This menu
 
