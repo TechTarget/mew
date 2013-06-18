c490810bc4dd5518613567d41894ed718a3b3083
demersdesigns <paul@demersdesigns.com>
2013-06-18 14:36:31 -0400

    change sequenceoptions to seqoptions

--- sites/dev/rimembracingmobilechaos2/javascript/init.js
+++ sites/dev/rimembracingmobilechaos2/javascript/init.js
@@ -32,7 +32,7 @@ $(document).ready(function ($) {
   });
 
   /* Animated Carousel */
-    var sequenceOptions = {
+    var seqOptions = {
         autoPlay: true,
         autoPlayDelay: 4000,
         pauseOnHover: true,
@@ -51,7 +51,7 @@ $(document).ready(function ($) {
         }
     };
 
-    var sequence = $("#sequence").sequence(sequenceOptions).data("sequence");
+    var sequence = $("#sequence").sequence(seqOptions).data("sequence");
 
     sequence.afterLoaded = function() {
         $(".navi").fadeIn(100);
